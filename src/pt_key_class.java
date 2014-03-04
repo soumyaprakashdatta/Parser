94ab7d1007777fdcc5f41f0b68861e14ef960b2b
Soumyaprakashdatta <dattasoumyaprakash@gmail.com>
2014-03-05 00:51:35 +0530

    All the rights associated with these files solely belong to Soumya Prakash Datta and Saikat Dutta , the authors.

---
+++ src/pt_key_class.java
@@ -0,0 +1,10 @@
+
+public class pt_key_class {
+    String terminal;
+    String non_terminal;
+
+    public pt_key_class(String terminal,String non_terminal){
+        this.terminal=terminal;
+        this.non_terminal=non_terminal;
+    }
+}
